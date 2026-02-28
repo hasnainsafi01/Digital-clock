adb77486e32e1514062cb2cbe2820b18dd27d19c
hasnainsafi01 <hasnainsafi444@gmail.com>
2026-03-01 00:00:05 +0500

    dc

---
+++ script.js
@@ -0,0 +1,28 @@
+let clock = document.getElementById("clock");
+let date = document.getElementById("date");
+setInterval(() => {
+// Time
+let now =new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+
+
+  hours=(hours % 12 === 0) ? 12 : hours % 12;
+  minutes=minutes<10 ? "0" + minutes :minutes;
+seconds= seconds<10 ? "0"+seconds:seconds
+
+// Date
+  let currentdate = now.getDate();
+  let month = now.getMonth() +1;
+  let year = now.getFullYear();
+
+ currentdate=currentdate<10 ? "0" +currentdate:currentdate;
+month=month<10?"0"+month:month;
+ 
+  clock.innerHTML = hours + ":" + minutes + ":" + seconds + " " + ampm;
+  date.innerHTML = currentdate + "/" + month + "/" + year;
+}, 1000);
+
